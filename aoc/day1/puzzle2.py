1f3258cad646e9c3b56894f8923a1840707a8218
Jouni Lammi <jjj.lammi@gmail.com>
2020-12-07 11:51:19 +0200

    flake8 fixes

--- aoc/day1/puzzle2.py
+++ aoc/day1/puzzle2.py
@@ -1,5 +1,5 @@
-import sys
 import traceback
+
 
 def read_data():
     with open("input.txt", "r") as handle:
@@ -8,6 +8,7 @@ def read_data():
     data = [i.strip("\n") for i in data]
     return data
 
+
 def search_occurrences(data):
     for i in data:
         for j in reversed(data):
@@ -15,13 +16,14 @@ def search_occurrences(data):
                 if int(i)+int(j)+int(k) == 2020:
                     return int(i), int(j), int(k)
 
+
 if __name__ == "__main__":
     try:
         data = read_data()
         i, j, k = search_occurrences(data)
         print(f"The numbers that have a sum of 2020 are {i}, {j}, {k}. "
               f"The answer to the question is {i*j*k}."
-        )
+              )
     except Exception:
         print("Please learn how to code")
         traceback.print_exc()
