a7a25618e9c21468fcf1865ff2e271430d8a1168
Jouni Lammi <jjj.lammi@gmail.com>
2020-12-23 00:14:37 +0200

    update readme, comment out day10 puzzle2

--- README.md
+++ README.md
@@ -5,9 +5,18 @@
 
 My solutions for [Advent of Code 2020.](https://www.adventofcode.com)
 
-When doing the solutions, I chose to go with clear and readable code instead of code golfing.
-While this may result in longer solutions, I believe that it will benefit me better in the long run and
-teach me more than code golfing, doing nasty hacks and writing everything in the main funcion.
+This was the second year that I participated in Advent of Code,
+and the first year that I actually put a notable effort in trying
+to solve the puzzles. Some were pretty easy, but some were very hard. There was a lot of 
+spaghetti code and bad design choices. But this year's puzzles taught me a lot. It was fun to think outside the box and
+get some variation to coding at work. But especially helpful was to go through 
+the solutions with others and compare approaches (shoutout to my colleague [Kalle Lämsä](https://github.com/coocos), 
+who was a huge help in getting a better understanding of the problems and their solutions). I also got a better 
+understanding of what I know and what I don't know, which gives a good start for learning in the future.
+
+
+I tried to keep the code readable and organized, but on some days all I could do was just to brute-force the solution 
+and pretty much leave the code as it is after that. If I have enough time at some point, I will refactor the solutions.
 
 The days are under the aoc/ folder. In each folder:
 - There is a separate puzzle1.py and puzzle2.py.
--- aoc/day10/puzzle2.py
+++ aoc/day10/puzzle2.py
@@ -3,78 +3,78 @@ import traceback
 # import itertools
 import copy
 from pprint import pprint
-WINDOW_SIZE = 25
-
-
-def read_data():
-    with open("input_.txt", "r") as handle:
-        data = [int(i.strip("\n")) for i in handle.readlines()]
-        data_set = set(data)
-        data_set.add(0)
-        data_set.add(max(data_set)+3)
-        print(data_set)
-    return data_set
-
-
-def parse_adapters(data):
-    sets = set()
-    sets.add(frozenset(data))
-    print(sets)
-    while True:
-        for idx, curr in enumerate(sets):
-            # print(idx)
-            # print()
-
-            curr = list(curr)
-            print(curr)
-            this_loop = []
-            for index, val in enumerate(list(curr)):
-                # print(val, list(curr))
-                try_list = copy.deepcopy(curr)
-                this_loop.append(frozenset(try_list))
-                # print(index, val)
-                if (index == 0) or (index == len(curr)-1):
-                    continue
-                if (curr[index + 1] - curr[index - 1]) <= 3:
-                    try_list = copy.deepcopy(curr)
-                    assert list(try_list) == list(curr)
-                    # if frozenset(try_list) not in sets:
-                    this_loop.append(frozenset(try_list))
-                    try_list.remove(val)
-                    # if frozenset(try_list) not in sets:
-                    this_loop.append(frozenset(try_list))
-                    # print(try_list)
-                    # print(sets)
-                    # print()
-                    # print(frozenset(curr))
-        # print(this_loop)
-        for i in this_loop:
-            sets.add(i)
-        this_loop = []
-        pprint(sets)
-        # print(len(sets))
-        input()
-        # one_jolt_diff = 0
-        # three_jolt_diff = 0
-        # current = 0
-        # while data:
-        #     diff = min(data) - current
-        #     if diff == 1:
-        #         one_jolt_diff += 1
-        #     elif diff == 3:
-        #         three_jolt_diff += 1
-        #     current = min(data)
-        #     data.remove(current)
-        # print(one_jolt_diff, three_jolt_diff +1)
-        # return one_jolt_diff, three_jolt_diff+1
-
-
-if __name__ == "__main__":
-    try:
-        data = read_data()
-        one_jolt, three_jolt = parse_adapters(data)
-        # assert first_faulty == 177777905
-        print(f"One-jolt difference multiplied by three-jolt differences is {one_jolt*three_jolt}")
-    except Exception:
-        print("Please learn how to code")
-        traceback.print_exc()
+# WINDOW_SIZE = 25
+#
+#
+# def read_data():
+#     with open("input_.txt", "r") as handle:
+#         data = [int(i.strip("\n")) for i in handle.readlines()]
+#         data_set = set(data)
+#         data_set.add(0)
+#         data_set.add(max(data_set)+3)
+#         print(data_set)
+#     return data_set
+#
+#
+# def parse_adapters(data):
+#     sets = set()
+#     sets.add(frozenset(data))
+#     print(sets)
+#     while True:
+#         for idx, curr in enumerate(sets):
+#             # print(idx)
+#             # print()
+#
+#             curr = list(curr)
+#             print(curr)
+#             this_loop = []
+#             for index, val in enumerate(list(curr)):
+#                 # print(val, list(curr))
+#                 try_list = copy.deepcopy(curr)
+#                 this_loop.append(frozenset(try_list))
+#                 # print(index, val)
+#                 if (index == 0) or (index == len(curr)-1):
+#                     continue
+#                 if (curr[index + 1] - curr[index - 1]) <= 3:
+#                     try_list = copy.deepcopy(curr)
+#                     assert list(try_list) == list(curr)
+#                     # if frozenset(try_list) not in sets:
+#                     this_loop.append(frozenset(try_list))
+#                     try_list.remove(val)
+#                     # if frozenset(try_list) not in sets:
+#                     this_loop.append(frozenset(try_list))
+#                     # print(try_list)
+#                     # print(sets)
+#                     # print()
+#                     # print(frozenset(curr))
+#         # print(this_loop)
+#         for i in this_loop:
+#             sets.add(i)
+#         this_loop = []
+#         pprint(sets)
+#         # print(len(sets))
+#         input()
+#         # one_jolt_diff = 0
+#         # three_jolt_diff = 0
+#         # current = 0
+#         # while data:
+#         #     diff = min(data) - current
+#         #     if diff == 1:
+#         #         one_jolt_diff += 1
+#         #     elif diff == 3:
+#         #         three_jolt_diff += 1
+#         #     current = min(data)
+#         #     data.remove(current)
+#         # print(one_jolt_diff, three_jolt_diff +1)
+#         # return one_jolt_diff, three_jolt_diff+1
+#
+#
+# if __name__ == "__main__":
+#     try:
+#         data = read_data()
+#         one_jolt, three_jolt = parse_adapters(data)
+#         # assert first_faulty == 177777905
+#         print(f"One-jolt difference multiplied by three-jolt differences is {one_jolt*three_jolt}")
+#     except Exception:
+#         print("Please learn how to code")
+#         traceback.print_exc()
